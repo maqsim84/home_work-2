5dd0cbbf9e155a123a0d31913282cf8f92f06c73
maqsim84 <51133152+maqsim84@users.noreply.github.com>
2019-05-31 09:51:05 +0500

    update template_folder='.'

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask
 from flask import render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 @app.route('/hello')
 @app.route('/hello/<name>')
 def hello(name=None):
